4162c19e6fa0a6712efc6faf7f1c743d003308db
Nutjaree11122222 <nutjareelungpon096@gmail.com>
2023-01-07 13:22:02 +0700

    edit index1

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <div class="container mt-5">
   <div class="row">
   <h1>Page Index</h1>
-  <h1>Page Server</h1>
+  <h1>Page for Server</h1>
+  <h1>Page for Server11111</h1>
   </div>
 </div>
 <?php include 'footer.php'?>
