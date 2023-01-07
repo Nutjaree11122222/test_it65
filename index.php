72f182b990d378de699530b4e41c9bfccd90894a
Nutjaree11122222 <nutjareelungpon096@gmail.com>
2023-01-07 10:24:26 +0700

    new many file

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php include 'demo.php'?>
+<?php include 'header.php';
+      include 'banner.php';
+      include 'navbar.php';
+?>
+<div>
+    <h1>Page Index</h1>
+</div>
+<?php include 'footer.php'?>
